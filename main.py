71ece4493c0ae318e0cdee770716231e2f94cfa0
FedericoSaitta <144010367+FedericoSaitta@users.noreply.github.com>
2023-09-21 23:14:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,3 @@ Fede = Account('Fede', 'data.csv', skip_line= True)
 
 Fede.data()
 Fede.graph()
-
-
-# Problems to fix
-
-# the new september bin is added to the old one, need to distinguish or make more bins that have the same name
-# better plotting ability and also graph should be prettier
-# plotting of the savings account
-# Better system for sifting through the different vendors that one does not want to include?
-# Also ability for the user inside main to choose the proper vendors
-# Also write about this stuff that you learned on the tablet
