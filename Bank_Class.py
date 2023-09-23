a5ddd23a90623da829f51e779a2af467f0d9e0d0
FedericoSaitta <144010367+FedericoSaitta@users.noreply.github.com>
2023-09-23 21:58:47 +0100

    Update Bank_Class.py

--- Bank_Class.py
+++ Bank_Class.py
@@ -1,117 +1,108 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 months = {
     '01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun',
     '07': 'Jul', '08': 'Aug', '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'
 }
 
-class Account:
 
+class Account:
     white_list = ['CARD_PAYMENT', 'TRANSFER', 'FEE', 'CARD_REFUND']
 
     black_list = ['To Univ Of Manchester', 'Depositing savings', 'Withdrawing savings',
-                   'Cavendish Place', 'Manchester Su', 'Sp Ratta Us', 'Uom Student Fee', 'To Apex Self Storage',
+                  'Cavendish Place', 'Manchester Su', 'Sp Ratta Us', 'Uom Student Fee', 'To Apex Self Storage',
                   'Stagecoach']
 
+    cache = []
 
-    def __init__(self, name, file, skip_line = False):
+    def __init__(self, name, file):
 
         self.months = []
-
         self.name = name
         self.file = file
-        self.skip_line = skip_line
 
-        self.payments = [] # This also contains positive transfers made to you
+    def make_objects(self, month, year):
+        name = month + year
+        name = Transactions(self.file, month, year)
+        self.months.append(name)
 
-        # To be implemented
-        self.savings = []
-        self.top_ups = []
-
-        self.cache = []  # THe cache should store specific payments that the user does not want to take into account
-
-        self.__compute() # When account is generated then it will automatically compute, maybe needs fixing
-
-
-    def get_transactions(self, month, year):
-        transactions = []
-        with open(self.file, 'r') as f:
-            reader = csv.reader(f)
-            if self.skip_line:
-                next(reader)
-
-            for row in reader:
-                if ((row[3][:7]).split('-')) == [year, month]:
-                    if row[0] in Account.white_list and (row[4] not in Account.black_list):
-                        if [row[5], (row[3][:10]).split('-'), row[4]] not in self.cache:
-                            transactions.append([row[5], (row[3][:10]).split('-'), row[4]])
-        return transactions
-
-    def exclude_transactions(self, *args):   # The args should be lists exclude_transaction( [], [], ...)
+    def exclude_transactions(self, obj, *args):  # The args should be lists exclude_transaction( [], [], ...)
         for arg in args:
-            if arg not in self.cache:
-                self.cache.append(arg)
+            if arg not in Account.cache:
+                Account.cache.append(arg)
+                obj.data.remove(arg)
+        obj.compute_daily_payments()
 
-    def make_attributes(self, month, year):
-        head = year + '/' + month
-        self.head = []
-        self.months.append(head)
-        return self.head
+    def graph(self, bar_chart=False):
+        self.sort_objs()
+        if bar_chart:
+            bins = []
+            y_values = []
 
-# This is a good start need to clean up the code significantly but this is a smart way to acces the possible month 
-# attributes without creating them too ahead of time
+            for obj in self.months:
+                y_values.append(sum(obj.y))
+                bins.append(months.get(obj.month) + ' ' + obj.year[2:])
+            plt.title(f'Total money spent by {self.name} in each month')
+            plt.bar(bins, y_values)
 
-    def set_set(self, month, year):
-        head = year + '/' + month
-        if head in self.months:
-            self.head.append(0)
-            return self.head
+        else:                               # creates a cumulative plot
+            for obj in self.months:
+                cumulative = np.cumsum(obj.y)
+                plt.plot(obj.x, cumulative, label=months.get(obj.month) + ' ' + obj.year[2:])
 
-# You could create account objects, each monthly span is an attribute, from that i can print each attribute and remove
-# any unecessay valuies and i can plot them side by side
-# I should also have a yearly graph as back up in general
+            plt.xlabel(f'Day of the month')
+            plt.legend(loc="upper left")
+            plt.title(f'Cumulative money by {self.name} over the month')
+
+        plt.show()
+
+    def sort_objs(self):
+        print(self.months)
+        self.months.sort(key=lambda x: int(x.month))
+        self.months.sort(key=lambda x: int(x.year))
 
 
-    def graph(self):  # I should also create a way to plot data just over the month
-        plt.bar(self.bins, self.yvalues)
-        plt.show()  # At one point change to saving the pictures in a correct width size
+class Transactions:
 
-    def __compute(self):
+    def __init__(self, file, month, year):
+
+        self.file = file
+        self.month = month
+        self.year = year
+
+        self.excluded_transactions = []
+
+
+
+        self.__get_transactions()
+        self.compute_daily_payments()
+
+    def compute_daily_payments(self):
+        self.x = [i for i in range(1, 32)]
+        self.y = [0 for i in self.x]
+        index = 0
+        for i in self.x:
+            while int(self.data[index][1][2]) == i:
+                self.y[i - 1] -= float(self.data[index][0])
+                if index == len(self.data) - 1:
+                    break
+                index += 1
+
+    def __get_transactions(self):
+        self.data = []
         with open(self.file, 'r') as f:
             reader = csv.reader(f)
-            if self.skip_line:
-                next(reader)
+            next(reader)                    # always skips the first line
 
             for row in reader:
-                if row[0] in Account.white_list and (row[4] not in Account.black_list):
-                    self.payments.append((float(row[5]) + float(row[6]), (row[3][:10]).split('-')))
+                if ((row[3][:7]).split('-')) == [self.year, self.month]:
+                    if row[0] in Account.white_list and (row[4] not in Account.black_list):
+                        self.data.append([row[5], (row[3][:10]).split('-'), row[4]])
+                    elif row[0] in Account.white_list:
+                        self.excluded_transactions.append([row[5], (row[3][:10]).split('-'), row[4]])
 
-          #      if (row[4] == 'Depositing savings' or row[4] == 'Withdrawing savings'):
-         #           self.savings.append( (float(row[5]) * -1, (row[3][:10]).split('-') ))
 
-
-    def yearly_data(self):
-        self.yvalues = []
-        self.bins = []
-        index = 0
-        lst = self.payments
-        j = 0
-        results = []
-
-        for i in range(13):  # the time period we want to assess
-            month = months.get(lst[j][1][1])
-            if i % 12 == 0:
-                self.bins.append(month + '' + lst[j][1][0])  # This works for now if graph is large enough width wise
-            else:
-                self.bins.append(month)
-
-            self.yvalues.append(0)
-
-            while lst[j][1][1] == lst[j + 1][1][1] and j < len(lst) - 2:
-                self.yvalues[i] += lst[j][0]
-                j += 1
-
-            self.yvalues[i] += lst[j][0]  # Done so the last element when the condition fails is also added to the list
-            self.yvalues[i] = self.yvalues[i] * -1 # Negating values as we look at money spent
-            j += 1            # Done so it does not get stuck and goes over to the next month
+    def __repr__(self):
+        return self.month + '/' + self.year
