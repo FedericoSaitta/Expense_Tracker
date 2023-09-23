fda86c1a3e8fad68f710db5dd66bc39ca344e769
FedericoSaitta <144010367+FedericoSaitta@users.noreply.github.com>
2023-09-23 21:59:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from Bank_Class import Account
-import numpy as np
 
-Fede = Account('Fede', 'data.csv', skip_line= True)
+# Create the Account object with the file with all the transactions
+Fede = Account('Fede', 'data.csv')
 
 
-Fede.data()
-Fede.graph()
+Fede.make_objects('09', '2023')
+Fede.make_objects('05', '2023')
+Fede.make_objects('03', '2023')
+Fede.make_objects('10', '2022')
+Fede.make_objects('09', '2022')
+
+Fede.graph(bar_chart= True)
