3e0fb5346e6e1fd8546cd2de5f160f0edc26168f
FedericoSaitta <144010367+FedericoSaitta@users.noreply.github.com>
2023-11-14 20:00:51 +0000

    Update main.py

--- Files/main.py
+++ Files/main.py
@@ -3,29 +3,12 @@ from Bank_Class import Account
 # Create the Account object with the file with all the transactions
 Fede = Account('Fede', 'data.csv')
 
-
+# Create a month object for each month that you want to track
 Fede.make_objects('09', '2023')
 Fede.make_objects('05', '2023')
 Fede.make_objects('03', '2023')
 Fede.make_objects('10', '2022')
 Fede.make_objects('09', '2022')
 
-
-
+# Plot the objects, together or separately
 Fede.graph(bar_chart= True)
-
-
-
-
-
-# program still assumes that all months have 31 days
-
-
-
-
-
-# Advanced:
-# Show vendors and signle transactions to be able to black_list them
-# Add trend line
-# implement interest rate with the time difference, note the savings values fluctuate
-# For this you will need to calculate time passed by looking at dates
